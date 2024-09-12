dd1ca9d10b588eb3424bff1535e084a505689c99
Manjari Mishra <manjarim5034@gmail.com>
2024-09-12 15:24:10 -0700

    js -looping  lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(names) {
+    let res = []
+    for (let i = 0; i< names.length; i++) {
+        res.push("Thank you, " + names[i] + ", for the wonderful surprise gift!")
+    }
+    return res
+}
+
+function countDown(cnt) {
+    let i = cnt
+    while (i-- >= 0) {
+        console.log(i + 1)
+    }
+}
+
+
+
